ce0fc20eb8cf145701bede1a18c9c9cafbbfd155
abhiarnikar <abhiarnikar725@gmail.com>
2022-09-19 11:39:43 +0530

    typescript

--- build/index3.js
+++ build/index3.js
@@ -18,3 +18,46 @@ function addition(x, y) {
 }
 addition(65, 45);
 addition('45', '14');
+// let a:number|string = "10"
+// let b:number|string|boolean = true
+let a = "10";
+let b = true;
+let info = {
+    firstName: "devayni",
+    lastName: "arnikar"
+};
+let info6 = {
+    mother: "sunanda",
+    father: "ashok"
+};
+// program5
+// let role = "read_only";
+// let role2 = "read_and_write";
+// let role3 = 'admin'
+var Roles;
+(function (Roles) {
+    Roles[Roles["read_only"] = 1] = "read_only";
+    Roles[Roles["read_and_write"] = 2] = "read_and_write";
+    Roles[Roles["admin"] = 3] = "admin";
+    Roles[Roles["customer"] = 4] = "customer";
+})(Roles || (Roles = {}));
+var Account;
+(function (Account) {
+    Account[Account["saving"] = 1] = "saving";
+    Account[Account["current"] = 2] = "current";
+})(Account || (Account = {}));
+if (Roles.read_only) {
+    console.log('reading only');
+}
+if (Roles.read_and_write) {
+    console.log('read and write ');
+}
+if (Roles.admin) {
+    console.log('admin');
+}
+if (Roles.customer) { // role == "customer"
+}
+if (Account.saving) {
+}
+if (Account.current) {
+}
--- src/index3.ts
+++ src/index3.ts
@@ -29,3 +29,69 @@ function addition(x: number|string,y:number|string){
 
 addition(65,45)
 addition('45','14')
+
+// Union
+type Result = number|string|boolean
+// let a:number|string = "10"
+// let b:number|string|boolean = true
+let a:Result = "10"
+let b:Result = true
+
+//literals
+
+type firstName = "abhijit"|"devayni"
+type lastName = "arnikar"|"deshpande"
+
+let info:{
+    firstName:firstName,
+    lastName:lastName
+}={
+    firstName:"devayni"
+    lastName:"arnikar"
+}
+
+// program
+
+type parent = {mother:string,father:string}
+
+let info6:parent = {
+    mother:"sunanda",
+    father:"ashok"
+}
+
+// program5
+
+// let role = "read_only";
+// let role2 = "read_and_write";
+// let role3 = 'admin'
+
+enum Roles {
+    "read_only"=1,
+    "read_and_write",
+    'admin',
+    'customer'
+}
+
+enum Account {
+    'saving'=1,
+    'current'
+}
+
+if(Roles.read_only){
+    console.log('reading only')
+}
+if(Roles.read_and_write){
+    console.log('read and write ')
+}
+if(Roles.admin){
+    console.log('admin')
+}
+if(Roles.customer){ // role == "customer"
+
+}
+if(Account.saving){
+
+}
+if(Account.current){
+    
+}
